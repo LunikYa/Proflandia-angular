5ad2f5d0240a1d1c592ede003e46a301013b20bd
Леонид Салтыков <leonidyarmak@gmail.com>
2018-05-23 02:48:40 +0300

    update api

--- server/router/security/securityRouters.js
+++ server/router/security/securityRouters.js
@@ -1,7 +1,11 @@
 const Router = require('koa-router');
 const router = Router();
-const { getLessons } = require('./dataController');
+const { getLessons,getProfessions,getUser,getChoosedProfessions} = require('./dataController');
 
-router.get('/lessons', getLessons);
+router.get('/lessons/:profession', getLessons);
+router.get('/professions', getProfessions);
+router.get('/user/:useremail', getUser);
+router.get('/choosed/professions/:usermail', getChoosedProfessions);
+// router.put('/choosed/professions/:usermail', putChoosedProfessions);
 
 module.exports = router;
