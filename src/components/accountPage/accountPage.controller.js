3be989af5c226bbfbf46b9563d8bc33dcfa68886
AGrigorovich <Alexey0195@gmail.com>
2018-06-04 13:25:49 +0300

    adding classes for styles of professions in accountPage, deleting the cleaning of the localStorage, adding a treatment of results of test on recommended professions and output results in the view

--- src/components/accountPage/accountPage.controller.js
+++ src/components/accountPage/accountPage.controller.js
@@ -1,14 +1,17 @@
 class AccountPageController {
-    constructor(AccountPageService, $location) {
+    constructor(AccountPageService, $rootScope) {
         this.AccountPageService = AccountPageService;
         this.exitToLocation = ('/');
         this.modalWindowText = 'Вы уверенны, что хотите выйти?';
-        this.$location=$location;
+        this.$rootScope=$rootScope;
+        this.$rootScope.recommendedProfession=this.$rootScope.recommendedProfession||[];
     }
 
     $onInit() {
         this.AccountPageService.getUserFromApi().then(res => this.user = res);
-        this.AccountPageService.getUserProfessionsAndLevelFromApi().then(res => this.userProfessions = console.log(res.professions));
+        this.AccountPageService.getUserProfessionsAndLevelFromApi().then(res => this.userProfessions = //res.professions);
+            [{"profession":"cook","currentLevel":2},{"profession":"designer","currentLevel":4},
+                {"profession":"teacher","currentLevel":6},{"profession":"builder","currentLevel":8},{"profession":"doctor","currentLevel":10}])
     }
 
     exitAccount() {
@@ -21,11 +24,6 @@ class AccountPageController {
 
     toCurrentProfession(currentProfession) {
         this.professionLessons = this.AccountPageService.getLessonsForProfessionFromApi(currentProfession).then(res => console.log(res))
-    }
-    $onDestroy(){
-        if(this.$location.$$path==='/') {
-            localStorage.clear();
-        }
     }
 }
 
--- src/components/accountPage/accountPage.service.js
+++ src/components/accountPage/accountPage.service.js
@@ -1,26 +1,25 @@
 import {ApiUrl} from "../../constants/constants";
 
-class AccountPageService{
-    constructor($http){
-        this.$http=$http;
-        this.userEmail=localStorage.getItem('useremail');
+class AccountPageService {
+    constructor($http) {
+        this.$http = $http;
+        this.userEmail = localStorage.getItem('useremail');
     }
-    getUserFromApi(){
-        return this.$http.get(ApiUrl+'/user/'+this.userEmail)
+
+    getUserFromApi() {
+        return this.$http.get(ApiUrl + '/user/' + this.userEmail)
             .then(response => response.data)
     }
-    getUserProfessionsAndLevelFromApi(){
-        return this.$http.get(ApiUrl+'/user/'+this.userEmail)
-            .then(response=>response.data)
+
+    getUserProfessionsAndLevelFromApi() {
+        return this.$http.get(ApiUrl + '/user/' + this.userEmail)
+            .then(response => response.data)
 
     }
-    getAllProfessionsFromApi(){
-        return this.userProfessions= this.$http.get(ApiUrl+'/professions')
-    }
 
-    getLessonsForProfessionFromApi(profession){
-        return this.userProfessions= this.$http.get(ApiUrl+'/lessons/'+profession)
-            .then(response=>response.data)
+    getLessonsForProfessionFromApi(profession) {
+        return this.userProfessions = this.$http.get(ApiUrl + '/lessons/' + profession)
+            .then(response => response.data)
     }
 }
 
