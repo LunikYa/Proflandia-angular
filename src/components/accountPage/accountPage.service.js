c650ea0854993d251d60197d9c305d05b62b27e1
AGrigorovich <Alexey0195@gmail.com>
2018-06-05 15:32:20 +0300

    change some data in accountPage

--- src/components/accountPage/accountPage.service.js
+++ src/components/accountPage/accountPage.service.js
@@ -1,26 +1,12 @@
 import {ApiUrl} from "../../constants/constants";
-
 class AccountPageService {
     constructor($http) {
         this.$http = $http;
         this.userEmail = localStorage.getItem('useremail');
     }
-
     getUserFromApi() {
         return this.$http.get(ApiUrl + '/user/' + this.userEmail)
             .then(response => response.data)
     }
-
-    getUserProfessionsAndLevelFromApi() {
-        return this.$http.get(ApiUrl + '/user/' + this.userEmail)
-            .then(response => response.data)
-
-    }
-
-    getLessonsForProfessionFromApi(profession) {
-        return this.userProfessions = this.$http.get(ApiUrl + '/lessons/' + profession)
-            .then(response => response.data)
-    }
 }
-
 export default AccountPageService
